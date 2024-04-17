bcdbd87fc3254cbceab2d58291c3126394090414
thoht3 <tho.hotrong@gmail.com>
2024-04-17 16:17:35 +0700

    xong 7 bai luon goi ne

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahren) {
+  return Math.round((fahren - 32) / 9 * 5 * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsi) {
+  return Math.round((celsi * 9 / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
